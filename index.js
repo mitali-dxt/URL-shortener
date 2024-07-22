5bbec094377649484849ec79a6766138bd99dca0
Mitali Dixit <mitali.dixit04@gmail.com>
2024-07-22 12:08:55 +0530

    connected to database

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const { connectToMongoDB } = require('./connect');
 const urlRoute = require('./routes/url');
 
 const app = express();
 const port = 8000;
+
+connectToMongoDB('mongodb://localhost:27017/url-shortener').then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+});
 
 app.use('/url', urlRoute);
 
