eb6c8ddbdf3d825fd9b9eb6ad2be4488201d24eb
Mitali Dixit <mitali.dixit04@gmail.com>
2024-12-28 00:23:33 +0530

    stateful to stateless auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,4 @@
 const User= require('../models/user');
-const { v4: uuidv4 } = require('uuid');
 const {setUser} = require('../service/auth');
 
 async function handleUserSignup(req,res){
@@ -18,9 +17,8 @@ async function handleUserLogin(req,res){
     if(!user) return res.render("login",{
         error:"Invalid username or password",
     });
-    const sessionId=uuidv4();
-    setUser(sessionId,user);
-    res.cookie('uid',sessionId);
+    const token = setUser(user);
+    res.cookie('uid',token);
     return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,20 @@
-const sessionIdToUserMap = new Map();
+//const sessionIdToUserMap = new Map(); no need to maintain state
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+const jwt = require('jsonwebtoken');
+const secretKey= "Mitali@1234"
+
+function setUser(user){
+    const payload = {
+        id:user.id,
+        email:user.email,
+    };
+    const token = jwt.sign(payload,secretKey);
+    return token;
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    return jwt.verify(token,secretKey);
 }
 
 module.exports ={
